f4b0d318915290c3ad6c3fbbd179e0d5964ce082
donny17-bit <doniwahyu14@gmail.com>
2021-01-26 02:31:39 +0700

    updated some view

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <title>Laravel</title>
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+
+<body>
+    <h1>
+        hello wolrd
+    </h1>
+</body>
+
+</html>
